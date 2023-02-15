2522773ae31469f3103d05c652b81c429025c559
Marti Congost <marti.congost@skyscanner.net>
2023-02-15 14:04:32 +0100

    Improved definition of default values in form schemas

--- modules/schema.js
+++ modules/schema.js
@@ -1,12 +1,32 @@
+export function getDefaults(schema) {
+  const defaults = {};
+  function traverse(elements) {
+    for (const element of elements) {
+      if (element instanceof FieldSet) {
+        traverse(element.fields);
+      } else {
+        defaults[element.name] = element.defaultValue;
+      }
+    }
+  }
+  traverse(schema);
+  return defaults;
+}
+
 export class Field {
-  constructor({ name, label, choices, visibility, input }) {
+  constructor({ name, label, defaultValue, choices, visibility, input }) {
     this.name = name;
     this.label = label;
+    if (defaultValue !== undefined) {
+      this.defaultValue = defaultValue;
+    }
     this.choices = choices;
     this.visibility = visibility;
     this.input = input;
   }
 }
+
+Field.prototype.defaultValue = null;
 
 export class FieldSet {
   constructor({ name, label, fields, visibility, icon }) {
@@ -24,14 +44,20 @@ export class BooleanField extends Field {
   }
 }
 
+BooleanField.prototype.defaultValue = false;
+
 export class StringField extends Field {
   constructor(props) {
     super(props);
   }
 }
 
+StringField.prototype.defaultValue = "";
+
 export class IntegerField extends Field {
   constructor(props) {
     super(props);
   }
 }
+
+IntegerField.prototype.defaultValue = 0;
