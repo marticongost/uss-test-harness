6447643620c043b6276e3f8127e8188249d2b581
Marti Congost <marti.congost@skyscanner.net>
2023-02-16 22:40:58 +0100

    Added the createElement function

--- modules/utils.js
+++ modules/utils.js
@@ -19,3 +19,19 @@ export function cloneElement(child, props) {
     ...props,
   });
 }
+
+export function createElement(element, props = null) {
+  if (!element) {
+    throw new Error(
+      "Empty value passed to createElement(); expecting a component function or a React element"
+    );
+  }
+  if (typeof element == "function") {
+    const ElementType = element;
+    return <ElementType {...props} />;
+  }
+  if (props) {
+    return cloneElement(element, props);
+  }
+  return element;
+}
