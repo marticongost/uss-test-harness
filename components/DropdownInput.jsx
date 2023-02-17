d7d5859adf6a3165e1c03527fb2ba30b4d529848
Marti Congost <marti.congost@skyscanner.net>
2023-02-17 17:38:33 +0100

    Added the DropdownInput component

---
+++ components/DropdownInput.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import ChevronDownIcon from "../images/chevron-down.svg";
+import { highlightColor, textBoxStyles } from "../modules/styles";
+import { createElement } from "../modules/utils";
+
+export default function DropdownInput({
+  field,
+  value,
+  onChange,
+  input,
+  label,
+  ...attributes
+}) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <div
+      css={{ display: "flex", flexDirection: "column", alignItems: "stretch" }}
+      {...attributes}
+    >
+      <button
+        type="button"
+        css={{
+          ...textBoxStyles,
+          display: "flex",
+          alignItems: "center",
+          textAlign: "left",
+          cursor: "pointer",
+          ":hover": {
+            borderColor: highlightColor,
+          },
+        }}
+        onClick={(e) => setExpanded(!expanded)}
+      >
+        {createElement(label || Label, { value, css: { flex: "1 1 auto" } })}
+        <ChevronDownIcon
+          css={{ height: "0.5rem", width: "auto", fill: "#333" }}
+        />
+      </button>
+      <div css={{ position: "relative" }}>
+        <div
+          css={[
+            {
+              position: "absolute",
+              opacity: 1,
+              transition: "opacity 0.2s ease",
+              boxShadow: "0 0 0.5rem rgba(0,0,0,0.2)",
+              backgroundColor: "white",
+              border: "1px solid #ddd",
+              padding: "1rem",
+              borderBottomLeftRadius: "0.3rem",
+              borderBottomRightRadius: "0.3rem",
+            },
+            !expanded && { opacity: 0, pointerEvents: "none" },
+          ]}
+        >
+          {createElement(input, { field, value, onChange })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Label({ value, ...attributes }) {
+  return (
+    <div {...attributes}>
+      {value !== null && value !== undefined ? value.toString() : null}
+    </div>
+  );
+}
