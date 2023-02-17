0537e424ea9d8d2c914d276a5f00916def350638
Marti Congost <marti.congost@skyscanner.net>
2023-02-17 17:39:37 +0100

    Added the utils.range function

--- modules/utils.js
+++ modules/utils.js
@@ -35,3 +35,27 @@ export function createElement(element, props = null) {
   }
   return element;
 }
+
+export function range(i, j, step = 1) {
+  if (!step) {
+    throw new Error("range() expected a non-zero step");
+  }
+  if (j === undefined || j == null) {
+    j = i;
+    i = 0;
+  }
+  let current, end;
+  if (step > 0) {
+    current = Math.min(i, j);
+    end = Math.max(i, j);
+  } else {
+    current = Math.max(i, j);
+    end = Math.min(i, j);
+  }
+  const values = [];
+  while (current != end) {
+    values.push(current);
+    current += step;
+  }
+  return values;
+}
