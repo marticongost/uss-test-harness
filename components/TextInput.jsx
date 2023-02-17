78b15fe215c47966eb42c6917ec4d3f560655bca
Marti Congost <marti.congost@skyscanner.net>
2023-02-17 17:39:00 +0100

    Code gardening in TextInput

--- components/TextInput.jsx
+++ components/TextInput.jsx
@@ -1,10 +1,11 @@
 import { textBoxStyles } from "../modules/styles";
 
-export default function TextInput({ field, onChange, ...attributes }) {
+export default function TextInput({ field, value, onChange, ...attributes }) {
   return (
     <input
-      name={field.name}
+      name={field?.name}
       css={textBoxStyles}
+      value={value}
       onChange={
         onChange ? (e) => onChange({ newValue: e.target.value || "" }) : null
       }
