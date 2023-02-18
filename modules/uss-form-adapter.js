6f428e15725b98fcd1c82fd58ae2b13a9b2a148f
Marti Congost <marti.congost@skyscanner.net>
2023-02-18 17:54:45 +0100

    Map the request form to a USS JSON request

--- modules/uss-form-adapter.js
+++ modules/uss-form-adapter.js
@@ -1,62 +1,137 @@
+import { EXPLORE_INTENT, inventories } from "./inventories";
+import { Anytime, DateRange, SingleDate, Month } from "./dates";
+import {
+  GEO_LEVEL_ANYWHERE,
+  GEO_LEVEL_COUNTRY,
+  GEO_LEVEL_LOCAL,
+  GEO_LEVEL_REGION,
+  GEO_LEVEL_POI,
+} from "./uss-geolevels";
+
 export function ussFormToRequest(formState) {
-  return {
-    requestContext: {
-      inventory: [
-        {
-          inventoryType: "SEARCH_INVENTORY_TYPE_FLIGHTS_INDICATIVE",
-          responseMessageDescriptor: {
-            messageDescriptorFullName:
-              "net.skyscanner.fluxcapacitor.taps.api.TapsUssResponseToC",
-          },
+  const response = { requestContext: ussFormToRequestContext(formState) };
+
+  if (formState.intent == EXPLORE_INTENT) {
+    response.exploreIntent = ussFormToExploreIntent(formState);
+  }
+
+  return response;
+}
+
+function ussFormToRequestContext(formState) {
+  const requestContext = { inventory: [] };
+  for (let inventory of inventories) {
+    if (!inventory.appliesToIntent(formState.intent)) {
+      continue;
+    }
+    if (formState[inventory.shorthand]) {
+      if (!inventory.descriptors?.length) {
+        console.error(
+          `No response message descriptor defined for inventory '${inventory.shorthand}' (have you set its 'descriptors' parameter?)`
+        );
+        continue;
+      }
+      requestContext.inventory.push({
+        inventoryType: inventory.searchInventoryType,
+        responseMessageDescriptor: {
+          messageDescriptorFullName: inventory.descriptors[0],
         },
-        {
-          inventoryType: "SEARCH_INVENTORY_TYPE_HOTELS_INDICATIVE",
-          responseMessageDescriptor: {
-            messageDescriptorFullName:
-              "net.skyscanner.bamboo.xsellv2.uss.TOCv1",
-          },
-        },
-        {
-          inventoryType: "SEARCH_INVENTORY_TYPE_TRAVEL_RESTRICTIONS",
-          responseMessageDescriptor: {
-            messageDescriptorFullName:
-              "net.skyscanner.martech.travelrestrictions.v2.USSExploreResponseTOC",
-          },
-        },
-        {
-          inventoryType: "SEARCH_INVENTORY_TYPE_LOCATION_IMAGES",
-          responseMessageDescriptor: {
-            messageDescriptorFullName:
-              "net.skyscanner.martech.postcard.v1.TOCv1",
-          },
-        },
-      ],
+      });
+    }
+  }
+  return requestContext;
+}
+
+function ussFormToExploreIntent(formState) {
+  const intent = {
+    travellerContext: {
+      market: formState.market,
+      locale: formState.locale,
+      currency: formState.currency,
     },
-    exploreIntent: {
-      travellerContext: {
-        market: "UK",
-        locale: "en-GB",
-        currency: "GBP",
-      },
-      origin: {
-        travelEntityId: "27544008",
-      },
-      destination: {
-        locations: {
-          geoLevel: "GEO_LEVEL_ANYWHERE",
-        },
-      },
-      targetGeoLevel: "GEO_LEVEL_COUNTRY",
-      travelDates: {
-        departureDate: {
-          anytime: true,
-        },
-        returnDate: {
-          anytime: true,
-        },
-      },
-    },
+    targetGeoLevel: formState.targetGeoLevel,
   };
+
+  if (formState.origin) {
+    intent.origin = { travelEntityId: formState.origin.id };
+  }
+
+  if (formState.destination) {
+    intent.destination = {
+      locations: {
+        geoLevel: fenryrLocationTypeToGeoLevel(formState.destination.type),
+        location: [{ travelEntityId: formState.destination.id }],
+      },
+    };
+  } else {
+    intent.destination = {
+      locations: { geoLevel: GEO_LEVEL_ANYWHERE },
+    };
+  }
+
+  intent.travelDates = {
+    departure: ussFormValueToFlexibleDate(formState.departure),
+  };
+  if (!formState.oneWay) {
+    intent.travelDates.return = ussFormValueToFlexibleDate(formState.return);
+  }
+  return intent;
+}
+
+function ussFormValueToFlexibleDate(formDate) {
+  if (formDate instanceof Anytime) {
+    return { anytime: true };
+  } else if (formDate instanceof Month) {
+    return {
+      date_range: {
+        range_start: {
+          year: formDate.year,
+          month: formDate.month,
+          day: 1,
+        },
+        range_end: {
+          year: formDate.year,
+          month: formDate.month,
+          day: new Date(formDate.year, formDate.month, 0).getDate(),
+        },
+      },
+    };
+  } else if (formDate instanceof SingleDate) {
+    return {
+      date: {
+        year: formDate.year,
+        month: formDate.month,
+        day: formDate.day,
+      },
+    };
+  } else if (formDate instanceof DateRange) {
+    return {
+      date_range: {
+        range_start: {
+          year: formDate.start.year,
+          month: formDate.start.month,
+          day: formDate.start.day,
+        },
+        range_end: {
+          year: formDate.end.year,
+          month: formDate.end.month,
+          day: formDate.end.day,
+        },
+      },
+    };
+  }
+}
+
+function fenryrLocationTypeToGeoLevel(type) {
+  if (type == "COUNTRY") {
+    return GEO_LEVEL_COUNTRY;
+  } else if (type == "REGION") {
+    return GEO_LEVEL_REGION;
+  } else if (type == "CITY" || type == "AIRPORT") {
+    return GEO_LEVEL_LOCAL;
+  }
+  // Assume POI for anything else, as this level matches dozens of canonical geo types
+  return GEO_LEVEL_POI;
 }
 
 export function ussFormFromRequest(request) {}
